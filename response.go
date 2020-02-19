caf948e6ebcb2c376afc389ff4f7909ef469b08a
awryme <awryme@gmail.com>
2020-02-19 20:34:07 +0300

    omit err

--- response.go
+++ response.go
@@ -1,7 +1,7 @@
 package main
 
 type Response struct {
-	Query int `json:"query",bson:"query"`
-	Resp int `json:"resp",bson:"resp"`
-	Err string `json:"err"`
+	Query int    `json:"query",bson:"query"`
+	Resp  int    `json:"resp",bson:"resp"`
+	Err   string `json:"err,omitempty"`
 }
